a8ae27b20511f81a8fd1198d1e84e377a3e502f7
pjy1368 <basejin0810@naver.com>
2021-03-16 11:37:09 +0900

    feat(StreamStudy): stream 실습2 수행

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -32,7 +32,10 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        return numbers.stream()
+            .filter(number -> number > 3)
+            .map(number -> 2 * number)
+            .reduce(0, Integer::sum);
     }
 
     public static void printLongestWordTop100() throws IOException {
